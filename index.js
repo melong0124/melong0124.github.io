4e1d258d2947be9df70e609cd672d4d6c1ea4255
melong0124 <cescokmtg@gmail.com>
2020-04-27 22:43:20 +0900

    커밋 테스트

---
+++ index.js
@@ -0,0 +1,10 @@
+new daum.roughmap.Lander({
+    "timestamp" : "1587994059902",
+    "key" : "y5sd",
+    "mapWidth" : "350",
+    "mapHeight" : "250"
+}).render();
+
+$(document).ready(function(){
+    $('.slider').slider();
+  });
