7c5dc726c9f6c9a47a60ffc4a89e77f7dfb83c25
melong0124 <melong0124@gmail.com>
2020-04-28 12:32:50 +0900

    최종

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var options = {
         "height": 350,
-        "interval": 2000
+        "interval": 4000
     }
     var instances = M.Slider.init(elems, options);
   });
